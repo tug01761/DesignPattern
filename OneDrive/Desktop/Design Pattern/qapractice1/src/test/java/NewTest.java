340a1bdb7eb63b6d72382a4c1a4e85fb693e79e1
Mike <tug01761@temple.edu>
2020-11-02 14:33:20 -0500

    adding files

---
+++ OneDrive/Desktop/Design Pattern/qapractice1/src/test/java/NewTest.java
@@ -0,0 +1,41 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+
+  @Test
+  public void newClassTest() {
+	  FirstTest test = new FirstTest();
+  }
+  
+  @Test
+  public void helloWorldTest() {
+	  System.out.println("Hello World!");
+  }
+  
+  @Test
+  public void tryCatchBlockTest() {
+	  FirstTest test = new FirstTest();
+	  test.isExist(5);
+  }
+  
+  @Test
+  public void test(){
+	  
+	  LocationBuilt location = LocationObjectMother.newyorkcity();
+	  
+  }
+  
+  
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+  
+  
+
+}
